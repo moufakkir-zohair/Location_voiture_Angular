3030054dbb4160f5e30bad45b58badb0aa525d52
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-06-07 22:00:21 +0100

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { CarsComponent } from './cars/cars.component';
 
 
 const routes: Routes = [
-
+  {path:"acceuil", component:AcceuilComponent},
+  {path:"cars", component:CarsComponent}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    CarsComponent,
   ],
   imports: [
     BrowserModule,
